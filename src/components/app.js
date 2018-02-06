f3919558002a056917b2968d4add7f206184d7d7
wrongeric <33172384+wrongeric@users.noreply.github.com>
2018-02-06 10:32:30 -0800

    new test

--- src/components/app.js
+++ src/components/app.js
@@ -1,13 +1,11 @@
 import React from 'react';
-import '../assets/css/app.css';
-import logo from '../assets/images/logo.svg';
+import GoogleMapComponent from './startingover/googleMapComponent';
+// import Map from './map';
+import {Container} from './startingover/container';
 
 const App = () => (
     <div>
-        <div className="app">
-            <img src={logo} className="logo rotate"/>
-            <h1>Welcome to React</h1>
-        </div>
+        <GoogleMapComponent />
     </div>
 );
 
